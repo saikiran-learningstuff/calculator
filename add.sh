4324b84658299420bfeaffa2b8ff8503c60d17c4
saikiran-learningstuff <225269119+saikiran-learningstuff@users.noreply.github.com>
2026-02-23 20:27:45 -0600

    Add test.sh in main
    Update echo statement in feature-add.sh
    
    Update echo statement in add.sh for main branch

--- add.sh
+++ add.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 
-echo "This is add function"
+<<<<<<< HEAD
+echo "This is add function - extension from feature branch"
+=======
+echo "This is add function - main branch"
+>>>>>>> d876c5f (Update echo statement in add.sh for main branch)
